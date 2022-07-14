895a439828d5b184257b80a3e4e0929a91fc96f9
Arif solanki <arifsolanki88@gmail.com>
2022-07-14 18:03:37 +0530

    diamond half

---
+++ Arif/diamond.js
@@ -0,0 +1,13 @@
+const pattern = () => {
+    for( let i =1; i <= 5; i++) {
+      let str = "";
+      for (let j=i; j<5; j++ ) {
+        str = str + " ";
+      }
+      for (let j = 1; j <= i; j++) {
+        str = str + "* ";
+      }
+      console.log(str);
+    }
+  };
+  pattern(); 
