5e5a65a01246c12d9eacccf1b2311642579c3330
Naouf <naouf123@gmail.com>
2022-07-15 17:18:10 +0530

    15july

--- Naouf/15july1.js
+++ Naouf/15july1.js
@@ -7,12 +7,9 @@ for (let i=m; i<=n; i++){
 pushvalue(5,29);
 console.log(arr);
 const filter = arr.filter((p) => {
-    for (i=1; i<=p; i++){
       if (p%10 ==7 ) {
         return true;
       }
-      }
-      return false;
     }
   )
   console.log(filter);
