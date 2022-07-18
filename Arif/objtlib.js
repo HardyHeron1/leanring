16cfd45b96399120f4a380f35340441f4627083e
MOHD OWAIS #WEB DEVELOPER <owaissiddiquione@gmail.com>
2022-07-18 15:40:06 +0530

    object library

---
+++ Arif/objtlib.js
@@ -0,0 +1,29 @@
+obj = {
+    objhistory: {
+        numberofbook: 3,
+        typeofbook: [1, 2, 3],
+        numberofbookissued: 3,
+        incharge: 'sam',
+    },
+    objgeography: {
+        numberofbook: 2,
+        typeofbook: [1, 2],
+        numberofbookissued: 1,
+        incharge: "Ary",
+    },
+    objmath: {
+        numberofbook: 5,
+        typeofbook: [1, 2, 3, 4, 5],
+        numberofbookissued: 4,
+        incharge: "nafy",
+    },
+    objscience: {
+        numberofbook: 1,
+        typeofbook: [1],
+        numberofbookissued: 2,
+        incharge: "nady",
+    },
+}
+console.log(obj.objhistory);
+
+
