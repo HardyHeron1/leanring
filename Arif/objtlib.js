43c5649fa5ce0d4b108ca088ebecf13601aafbf8
MOHD OWAIS #WEB DEVELOPER <owaissiddiquione@gmail.com>
2022-07-18 15:47:11 +0530

    object library

--- Arif/objtlib.js
+++ Arif/objtlib.js
@@ -1,29 +1,29 @@
-obj = {
-    objhistory: {
+const library = {
+    history: {
         numberofbook: 3,
         typeofbook: [1, 2, 3],
         numberofbookissued: 3,
         incharge: 'sam',
     },
-    objgeography: {
+    geography: {
         numberofbook: 2,
         typeofbook: [1, 2],
         numberofbookissued: 1,
         incharge: "Ary",
     },
-    objmath: {
+    math: {
         numberofbook: 5,
         typeofbook: [1, 2, 3, 4, 5],
         numberofbookissued: 4,
         incharge: "nafy",
     },
-    objscience: {
+    science: {
         numberofbook: 1,
         typeofbook: [1],
         numberofbookissued: 2,
         incharge: "nady",
     },
 }
-console.log(obj.objhistory);
+console.log(library.history);
 
 
