fce708629bf0edc4acd5491245a08cd0fe1409ff
MOHD OWAIS #WEB DEVELOPER <owaissiddiquione@gmail.com>
2022-07-21 17:29:48 +0530

    18th july and 19th july assignment

---
+++ Nadeem/fact.js
@@ -0,0 +1,76 @@
+const data = [
+    {
+        fact: "Unlike dogs, cats do not have a sweet tooth. Scientists believe this is due to a mutation in a key taste receptor.",
+        length: 114
+    },
+    {
+        fact: "When a cat chases its prey, it keeps its head level. Dogs and humans bob their heads up and down.",
+        length: 97
+    },
+    {
+        fact: "The technical term for a cat’s hairball is a “bezoar.”",
+        length: 54
+    },
+    {
+        fact: "A group of cats is called a “clowder.”",
+        length: 38
+    },
+    {
+        fact: "A cat can’t climb head first down a tree because every claw on a cat’s paw points the same way. To get down from a tree, a cat must back down.",
+        length: 142
+    },
+    {
+        fact: "Cats make about 100 different sounds. Dogs make only about 10.",
+        length: 62
+    },
+    {
+        fact: "Every year, nearly four million cats are eaten in Asia.",
+        length: 55
+    },
+    {
+        fact: "There are more than 500 million domestic cats in the world, with approximately 40 recognized breeds.",
+        length: 100
+    },
+    {
+        fact: "Approximately 24 cat skins can make a coat.",
+        length: 43
+    },
+    {
+        fact: "While it is commonly thought that the ancient Egyptians were the first to domesticate cats, the oldest known pet cat was recently found in a 9,500-year-old grave on the Mediterranean island of Cyprus. This grave predates early Egyptian art depicting cats by 4,000 years or more.",
+        length: 278
+    },
+]
+
+const arr = [];
+for (i=0; i<data.length; i++){
+    arr.push(data[i].fact);
+}
+console.log(arr);
+
+let val = 0;
+for (i=0; i<data.length; i++){
+    val = val + data[i].fact.length;
+}
+console.log(val);
+
+const arr1 = arr.filter((val) => {
+    if (val.indexOf("down") !== -1){
+        return true;
+    }
+})
+console.log(arr1);
+console.log(data.fact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
